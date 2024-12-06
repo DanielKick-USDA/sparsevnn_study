84531e56262614dcb414f90b3d6ea94512b39e11
daniel.kick <daniel.kick@usda.gov>
2024-12-06 09:25:10 -0600

    Updated to reference correct container

--- data_Example/phno_Example/lin/gapit_helper.py
+++ data_Example/phno_Example/lin/gapit_helper.py
@@ -9,10 +9,11 @@ o  = [o[i] for i in range(len(o)) if oc[i] > min(oc)]
 o = [e.split('_')[0] for e in o]
 # convert into commands
 o = [' '.join([
-'singularity exec ../../../containers/new.sif Rscript gapit_cli.R',
+'singularity exec ../../../containers/gapit.sif Rscript gapit_cli.R',
 f"--phno_file '../../shared_data/{os.getcwd().split('/')[-2]}.csv'",
 # assume there is only one hapmap
 f"--geno_file '../../shared_data/{[e for e in os.listdir('../../shared_data/') if re.findall(r'hmp.txt', e)][0]}'",
+f"--max_snps 0",
 f"--hash {e}",
 "--hash_dir '../dnn/vnn_cache/'",
 '--out_dir ./models/gwas/'
